746432418c7cd25c78f85f97dfc178fe1ae63e70
Neil <neilrowland56@yahoo.com>
2021-12-17 06:30:42 +0000

    Updated .env file and next.config.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -10,5 +10,4 @@ const dbConnect = () => {
 		useUnifiedTopology: true,
 	});
 };
-
 export default dbConnect;
--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
 
 module.exports = {
 	env: {
-		DB_LOCAL_URI: 'mongodb://localhost:27017/bookit77',
+		DB_LOCAL_URI: process.env.DB_LOCAL_URI,
 	},
 };
