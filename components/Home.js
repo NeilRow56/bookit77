6b3c206ed3771a4e5d3345d454f729b7ee5a9cf7
Neil <neilrowland56@yahoo.com>
2021-12-20 06:06:29 +0000

    Added Pagination

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,17 @@
 import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Pagination from 'react-js-pagination';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
-import { Text, Container, Box, Link, Wrap } from '@chakra-ui/react';
+import {
+	Text,
+	Container,
+	Box,
+	Link,
+	Wrap,
+	Flex,
+	Center,
+} from '@chakra-ui/react';
 import RoomItem from './room/RoomItem';
 
 import { toast } from 'react-toastify';
@@ -11,7 +21,12 @@ import { clearErrors } from '../redux/actions/roomActions';
 
 export default function Home() {
 	const dispatch = useDispatch();
-	const { rooms, error } = useSelector((state) => state.allRooms);
+	const router = useRouter();
+	const { rooms, resPerPage, roomsCount, filteredRoomsCount, error } =
+		useSelector((state) => state.allRooms);
+
+	let { page = 1 } = router.query;
+	page = Number(page);
 
 	useEffect(() => {
 		if (error) {
@@ -20,28 +35,56 @@ export default function Home() {
 		}
 	}, []);
 
+	const handlePagination = (pageNumber) => {
+		router.push(`/?page=${pageNumber}`);
+	};
+
 	return (
-		<Container maxW="100%" overflow="hidden" align="center">
-			<Text color="#cc0000" mb="30px" mt="30px" fontSize={32}>
-				Stays in New York
-			</Text>
-			<Box marginBottom="20px" align="Start">
-				<ArrowBackIcon color="secondary" />{' '}
-				<NextLink href="/" passHref>
-					<Link color="secondary" style={{ textDecoration: 'none' }}>
-						Back to search
-					</Link>
-				</NextLink>
-			</Box>
-			<Wrap spacing="20px" justify="center">
-				{rooms && rooms.length === 0 ? (
-					<Box alert alert-danger>
-						<Text>No Rooms Found</Text>
-					</Box>
-				) : (
-					rooms.map((room) => <RoomItem key={room._id} room={room} />)
+		<>
+			<Container maxW="100%" overflow="hidden" align="center">
+				<Text color="#cc0000" mb="30px" mt="30px" fontSize={32}>
+					Stays in New York
+				</Text>
+				<Box marginBottom="20px" align="Start">
+					<ArrowBackIcon color="secondary" />{' '}
+					<NextLink href="/" passHref>
+						<Link
+							color="secondary"
+							style={{ textDecoration: 'none' }}
+						>
+							Back to search
+						</Link>
+					</NextLink>
+				</Box>
+				<Wrap spacing="20px" justify="center">
+					{rooms && rooms.length === 0 ? (
+						<Box alert alert-danger>
+							<Text>No Rooms Found</Text>
+						</Box>
+					) : (
+						rooms.map((room) => (
+							<RoomItem key={room._id} room={room} />
+						))
+					)}
+				</Wrap>
+				<br />
+				{resPerPage < roomsCount && (
+					<Flex justifyContent="center">
+						<Pagination
+							activePage={page}
+							itemsCountPerPage={resPerPage}
+							totalItemsCount={roomsCount}
+							onChange={handlePagination}
+							nextPageText={'Next'}
+							prevPageText={'Prev'}
+							firstPageText={'First'}
+							lastPageText={'Last'}
+							itemClass="page-item"
+							linkClass="page-link"
+						/>
+					</Flex>
 				)}
-			</Wrap>
-		</Container>
+			</Container>
+		</>
 	);
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import '../styles/globals.css';
 import { wrapper } from '../redux/store';
 
 function MyApp({ Component, pageProps }) {
